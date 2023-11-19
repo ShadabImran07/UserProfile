3b46aea5c69001d4c16edc8ac9ccefba64a6b302
ShadabImran07 <shadabimran07@gmail.com>
2023-11-20 01:36:55 +0530

    done some chages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -15,12 +15,11 @@ const App = () => {
 	return (
 		<BrowserRouter>
 			<header className='w-full flex justify-between items-center bg-white sm:px-8 px-4 py-4 border-b border-b-[#e6ebf4]'>
-				<Link to='/'>
-					<img
-						src={logo}
-						alt='logo'
-						className='w-28 object-contain'
-					/>
+				<Link
+					to='/'
+					className='font-bold'
+				>
+					Heliverse
 				</Link>
 				<div className='flex gap-5'>
 					<Link
